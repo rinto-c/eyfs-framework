a9a8ae39a357e59c316835f0bc316c43e3b5f9cc
Rinto Cyriac <rinto.cyriac@digital.education.gov.uk>
2021-01-07 10:09:10 +0000

    changes to menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,3 +28,9 @@ const subLinks = document.querySelector('.app-subnav--mobile .app-subnav__link')
 subLinks.onclick = () => {
   subLinks.classList.toggle('is-active');
 }
+
+const subLinks2 = document.querySelector('.app-subnav--mobile .app-subnav__link--section-2');
+
+subLinks2.onclick = () => {
+  subLinks2.classList.toggle('is-active');
+}
