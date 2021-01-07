2c18b84747d50e1921158993c417c92d5ee2c875
Jesse Yuen <hello@jesseyuen.com>
2021-01-07 09:54:19 +0000

    Extended the js to accommodate multiple sub headings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,17 +14,27 @@ $(document).ready(function () {
 // // Initialise mobile navigation
 // new MobileNav().init()
 
-
+// Open/close mobile nav
 const menuButton = document.querySelector('.js-app-mobile-nav-toggler');
 const mobileSubNav = document.querySelector('.app-subnav--mobile');
 
 menuButton.onclick = () => {
   menuButton.classList.toggle('is-active');
   mobileSubNav.classList.toggle('is-open');
+
+  // TODO: Close all the subLinks sections when we close the mobile nav
 }
 
-const subLinks = document.querySelector('.app-subnav--mobile .app-subnav__link');
+// Sub navigation
 
-subLinks.onclick = () => {
-  subLinks.classList.toggle('is-active');
-}
+// Loop through and find all the sub headings
+const subLinks = document.querySelectorAll('.app-subnav--mobile .app-subnav__link');
+
+// build an array of the subLinks
+Array.from(subLinks).forEach(link => {
+    // listen for a click on subLinks
+    link.addEventListener('click', function(event) {
+      // target the speficic subLink that was clicked
+      this.classList.toggle('is-active');
+    });
+});
