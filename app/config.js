37369c3458486feb81997b04ef4e2731639b699a
Steve Fox <stevefoxcs@gmail.com>
2021-01-20 09:41:36 +0000

    Directory page added

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get help with EYFS',
+  serviceName: 'Help for early years providers',
 
   // Default port that prototype runs on
   port: '3000',
