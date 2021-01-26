1639ea03d6174b726946f5b7e823db7c4cb36b59
Rinto Cyriac <rinto.cyriac@digital.education.gov.uk>
2021-01-26 11:55:11 +0000

    Update Maths and Counting page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -115,3 +115,14 @@ subNavSeven.onclick = () => {
 //     this.classList.toggle('app-mobile-nav__subnav--active');
 //   });
 // });
+
+// Loops through dom and finds all elements with nhsuk-card--clickable class
+document.querySelectorAll('.nhsuk-card--clickable').forEach((panel) => {
+  // Check if panel has a link within it
+  if (panel.querySelector('a') !== null) {
+    // Clicks the link within the heading to navigate to desired page
+    panel.addEventListener('click', () => {
+      panel.querySelector('a').click();
+    });
+  }
+});
