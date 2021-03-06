4f2067537182e30944ab1a0dcbab47465d260158
Rinto Cyriac <rinto.cyriac@digital.education.gov.uk>
2021-03-06 00:49:38 +0000

    Clean up the cards classes and styles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -152,3 +152,14 @@ document.querySelectorAll('.nhsuk-card--clickable').forEach((panel) => {
     });
   }
 });
+
+// Loops through dom and finds all elements with nhsuk-card--clickable class
+document.querySelectorAll('.eyfs-card--clickable').forEach((panel) => {
+  // Check if panel has a link within it
+  if (panel.querySelector('a') !== null) {
+    // Clicks the link within the heading to navigate to desired page
+    panel.addEventListener('click', () => {
+      panel.querySelector('a').click();
+    });
+  }
+});
