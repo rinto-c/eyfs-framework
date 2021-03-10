9f9ceaaf39e887486969b4b5aa04bc8de32c2ae4
Rinto Cyriac <rinto.cyriac@digital.education.gov.uk>
2021-03-10 19:35:22 +0000

    Shorten the block of js for subnav

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -41,92 +41,92 @@ stickyMenu.onclick = () => {
 
 
 // // Open/close sub nav mobile
-const subNavOne = document.querySelector('.topic-one');
-const subNavItemOne = document.querySelector('.app-mobile-nav__subnav--topic-one');
+// const subNavOne = document.querySelector('.topic-one');
+// const subNavItemOne = document.querySelector('.app-mobile-nav__subnav--topic-one');
 
-subNavOne.onclick = () => {
-  subNavOne.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemOne.classList.toggle('app-mobile-nav__subnav--active');
+// subNavOne.onclick = () => {
+//   subNavOne.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemOne.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavTwo = document.querySelector('.topic-two');
-const subNavItemTwo = document.querySelector('.app-mobile-nav__subnav--topic-two');
+// // // Open/close sub nav mobile
+// const subNavTwo = document.querySelector('.topic-two');
+// const subNavItemTwo = document.querySelector('.app-mobile-nav__subnav--topic-two');
 
-subNavTwo.onclick = () => {
-  subNavTwo.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemTwo.classList.toggle('app-mobile-nav__subnav--active');
+// subNavTwo.onclick = () => {
+//   subNavTwo.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemTwo.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavThree = document.querySelector('.topic-three');
-const subNavItemThree = document.querySelector('.app-mobile-nav__subnav--topic-three');
+// // // Open/close sub nav mobile
+// const subNavThree = document.querySelector('.topic-three');
+// const subNavItemThree = document.querySelector('.app-mobile-nav__subnav--topic-three');
 
-subNavThree.onclick = () => {
-  subNavThree.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemThree.classList.toggle('app-mobile-nav__subnav--active');
+// subNavThree.onclick = () => {
+//   subNavThree.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemThree.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavFour = document.querySelector('.topic-four');
-const subNavItemFour = document.querySelector('.app-mobile-nav__subnav--topic-four');
+// // // Open/close sub nav mobile
+// const subNavFour = document.querySelector('.topic-four');
+// const subNavItemFour = document.querySelector('.app-mobile-nav__subnav--topic-four');
 
-subNavFour.onclick = () => {
-  subNavFour.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemFour.classList.toggle('app-mobile-nav__subnav--active');
+// subNavFour.onclick = () => {
+//   subNavFour.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemFour.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavFive = document.querySelector('.topic-five');
-const subNavItemFive = document.querySelector('.app-mobile-nav__subnav--topic-five');
+// // // Open/close sub nav mobile
+// const subNavFive = document.querySelector('.topic-five');
+// const subNavItemFive = document.querySelector('.app-mobile-nav__subnav--topic-five');
 
-subNavFive.onclick = () => {
-  subNavFive.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemFive.classList.toggle('app-mobile-nav__subnav--active');
+// subNavFive.onclick = () => {
+//   subNavFive.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemFive.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavSix = document.querySelector('.topic-six');
-const subNavItemSix = document.querySelector('.app-mobile-nav__subnav--topic-six');
+// // // Open/close sub nav mobile
+// const subNavSix = document.querySelector('.topic-six');
+// const subNavItemSix = document.querySelector('.app-mobile-nav__subnav--topic-six');
 
-subNavSix.onclick = () => {
-  subNavSix.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemSix.classList.toggle('app-mobile-nav__subnav--active');
+// subNavSix.onclick = () => {
+//   subNavSix.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemSix.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavSeven = document.querySelector('.topic-seven');
-const subNavItemSeven = document.querySelector('.app-mobile-nav__subnav--topic-seven');
+// // // Open/close sub nav mobile
+// const subNavSeven = document.querySelector('.topic-seven');
+// const subNavItemSeven = document.querySelector('.app-mobile-nav__subnav--topic-seven');
 
-subNavSeven.onclick = () => {
-  subNavSeven.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemSeven.classList.toggle('app-mobile-nav__subnav--active');
+// subNavSeven.onclick = () => {
+//   subNavSeven.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemSeven.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
-// // Open/close sub nav mobile
-const subNavEight = document.querySelector('.topic-eight');
-const subNavItemEight = document.querySelector('.app-mobile-nav__subnav--topic-eight');
+// // // Open/close sub nav mobile
+// const subNavEight = document.querySelector('.topic-eight');
+// const subNavItemEight = document.querySelector('.app-mobile-nav__subnav--topic-eight');
 
-subNavEight.onclick = () => {
-  subNavEight.classList.toggle('app-mobile-nav__subnav-toggler--active');
-  subNavItemEight.classList.toggle('app-mobile-nav__subnav--active');
+// subNavEight.onclick = () => {
+//   subNavEight.classList.toggle('app-mobile-nav__subnav-toggler--active');
+//   subNavItemEight.classList.toggle('app-mobile-nav__subnav--active');
 
-  // TODO: Close all the subLinks sections when we close the mobile nav
-}
+//   // TODO: Close all the subLinks sections when we close the mobile nav
+// }
 
 // // Sub navigation
 
@@ -145,6 +145,22 @@ subNavEight.onclick = () => {
 //     this.nextElementSibling('.app-mobile-nav__subnav').toggle('govuk-visually-hidden')
 //   });
 // });
+
+// Block of script to replace the multiple blocks to toggle the sublinks within the mobile menu – Jesse to review
+const subLinks = document.querySelectorAll('.app-mobile-nav-subnav-toggler');
+console.log(subLinks.nextElementSibling)
+// build an array of the subLinks
+Array.from(subLinks).forEach(link => {
+  // listen for a click on subLinks
+  link.addEventListener('click', function(event) {
+  console.log('clicked')
+    // target the speficic subLink that was clicked
+    this.classList.toggle('app-mobile-nav__subnav--active');
+    e.target.nextSibling.classList.toggle('govuk-visually-hidden');
+  });
+});
+
+
 
 // Loops through dom and finds all elements with nhsuk-card--clickable class
 document.querySelectorAll('.nhsuk-card--clickable').forEach((panel) => {
